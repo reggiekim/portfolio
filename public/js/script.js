7367719917b4858d89a2d72ec244330b042e91d0
Reggie Kim <reggiekim@gmail.com>
2016-09-19 20:47:36 -0400

    add scrollfire and pushpin

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,14 @@
 "use strict";
 (function(){
-  console.log("jquery and script file loaded")
+  console.log("jquery and script file loaded");
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+  $(document).ready(function(){
+    $('#nav-container').pushpin({ top: $('#nav-container').offset().top });
+  });
+
 
 })();
